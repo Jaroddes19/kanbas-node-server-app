382f9bfc15dc6e11920ca084f3b62c3d2ff41d67
JarodD19 <113318528+JarodD19@users.noreply.github.com>
2024-04-02 00:53:19 -0400

    Deploying a5

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import Hello from "./Hello.js"
+import Lab5 from "./Lab5.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import cors from "cors";
 const express = require('express')
 const app = express()
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
-app.listen(4000)
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app)
+app.listen(process.env.PORT || 4000);
